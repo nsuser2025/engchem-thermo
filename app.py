aef407f701b5292dbe3ac1c26b4af87762775a4d
nsuser2025 <ryosuke_ishizuka@shokubai.co.jp>
2025-09-30 12:38:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import numpy as np
 page = st.selectbox("計算を選択してください", ["SAFT", "KD viscosity", "Zkanics"])
 
 if page == "SAFT":
-    st.header("SAFT")
-    st.write("ここはホームです")
+    st.header("SAFT型状態方程式")
+    st.write("建設中！注意！")
 
 elif page == "KD viscosity":
     st.header("Krieger-Dougherty粘性推算")
@@ -16,34 +16,33 @@ elif page == "KD viscosity":
     phi_max_1 = st.number_input("最大充填体積分率（粒子1を隙間なく詰めたときの上限, def. 0.58）", value=0.58)
     phi_max_2 = st.number_input("最大充填体積分率（粒子2を隙間なく詰めたときの上限, def. 0.58）", value=0.58)
     bool_comp = st.checkbox("粒子1と2を比較しますか?")
-   if bool_comp:
+    if bool_comp:
        st.write("粒子2のグラフを表示します")
-   else:
+    else:
        st.write("粒子2のグラフは非表示です")
     
-    
 elif page == "Zkanics":
-    st.title("結果ページ")
-    st.write("ここに計算結果やグラフを表示")
+    st.title("なにをつくるのか考え中！")
+    st.write("しましましまうま～")
 
-st.title("サンプル")
-st.write("上から順に表示されます")
-st.button("ボタン1")
-st.button("ボタン2")
+#st.title("サンプル")
+#st.write("上から順に表示されます")
+#st.button("ボタン1")
+#st.button("ボタン2")
 
-st.title("Hello World")
-st.write("これは Streamlit で表示しているテキストです！")
+#st.title("Hello World")
+#st.write("これは Streamlit で表示しているテキストです！")
 
 # データ作成
-x = np.linspace(0, 10, 100)
-y = np.sin(x)
+#x = np.linspace(0, 10, 100)
+#y = np.sin(x)
 
 # Matplotlib でプロット
-fig, ax = plt.subplots()
-ax.plot(x, y)
-ax.set_title("Sin Wave")
-ax.set_xlabel("x")
-ax.set_ylabel("y")
+#fig, ax = plt.subplots()
+#ax.plot(x, y)
+#ax.set_title("Sin Wave")
+#ax.set_xlabel("x")
+#ax.set_ylabel("y")
 
 # Streamlit に表示
-st.pyplot(fig)
+#st.pyplot(fig)
