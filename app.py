5386b403bf8a871f2bf2cd4fdb0485adae9a6635
nsuser2025 <ryosuke_ishizuka@shokubai.co.jp>
2025-09-30 12:04:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,20 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
 
+page = st.selectbox("ページを選択してください", ["ホーム", "設定", "結果"])
+
+if page == "ホーム":
+    st.title("ホームページ")
+    st.write("ここはホームです")
+
+elif page == "設定":
+    st.title("設定ページ")
+    st.number_input("パラメータを入力", value=10)
+
+elif page == "結果":
+    st.title("結果ページ")
+    st.write("ここに計算結果やグラフを表示")
+
 st.title("サンプル")
 st.write("上から順に表示されます")
 st.button("ボタン1")
