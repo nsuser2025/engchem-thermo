6491d4ae1a81301103c9f86b1f95d5247d08571d
nsuser2025 <ryosuke_ishizuka@shokubai.co.jp>
2025-09-30 13:03:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ if page == "SAFT EoS":
 
 elif page == "KD Viscosity":
     st.header("Krieger-Dougherty粘性推算")
+    plot_placeholder = st.empty()
     eta0 = st.number_input("基材粘度 [mPa・s]（def. エポキシ樹脂単体の粘度 1000）", value=1000)
     eta_intrinsic = st.number_input("固有粘度（形状依存性, def. 球体2.5）", value=2.5)
     phi_max_1 = st.number_input("最大充填体積分率（粒子1を隙間なく詰めたときの上限, def. 0.58）", value=0.58)
@@ -35,7 +36,7 @@ elif page == "KD Viscosity":
     if bool_comp:
        eta_2 = kd_viscosity(phi, eta0, eta_intrinsic, phi_max_2)
 
-    plot_placeholder = st.empty()
+    
     
     if st.button("実行"):
        # プロット
