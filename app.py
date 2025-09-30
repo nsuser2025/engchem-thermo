3c042bb6170185c911a7f809e0ac476fa6539e3e
nsuser2025 <ryosuke_ishizuka@shokubai.co.jp>
2025-09-30 11:58:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,21 @@
 import streamlit as st
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
 
 st.title("Hello World")
 st.write("これは Streamlit で表示しているテキストです！")
+
+# データ作成
+x = np.linspace(0, 10, 100)
+y = np.sin(x)
+
+# Matplotlib でプロット
+fig, ax = plt.subplots()
+ax.plot(x, y)
+ax.set_title("Sin Wave")
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+
+# Streamlit に表示
+st.pyplot(fig)
