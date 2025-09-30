fdf58538f7748807b8655e03eb12ca2420bb8075
nsuser2025 <ryosuke_ishizuka@shokubai.co.jp>
2025-09-30 12:42:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
 
-page = st.selectbox("計算を選択してください", ["SAFT", "KD viscosity", "Zkanics"])
+page = st.selectbox("計算を選択してください", ["SAFT EoS", "KD Viscosity", "ODE Solver"])
 
-if page == "SAFT":
+if page == "SAFT EoS":
     st.header("SAFT型状態方程式")
     st.write("建設中！注意！")
 
-elif page == "KD viscosity":
+elif page == "KD Viscosity":
     st.header("Krieger-Dougherty粘性推算")
     eta0 = st.number_input("基材粘度 [mPa・s]（def. エポキシ樹脂単体の粘度 1000）", value=1000)
     eta_intrinsic = st.number_input("固有粘度（形状依存性, def. 球体2.5）", value=2.5)
@@ -25,9 +25,9 @@ elif page == "KD viscosity":
     else:
        st.write("粒子2のグラフは非表示です")
     
-elif page == "Zkanics":
-    st.title("なにをつくるのか考え中！")
-    st.write("しましましまうま～")
+elif page == "ODE Solvedr":
+    st.header("常微分方程式（ODE）ソルバー")
+    st.write("SCIPY")
 
 #st.title("サンプル")
 #st.write("上から順に表示されます")
