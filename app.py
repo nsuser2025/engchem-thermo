4498aeab2376cfc56bf791fe794db91478914f7c
nsuser2025 <ryosuke_ishizuka@shokubai.co.jp>
2025-09-30 12:55:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -53,25 +53,3 @@ elif page == "KD Viscosity":
 elif page == "ODE Solvedr":
     st.header("常微分方程式（ODE）ソルバー")
     st.write("SCIPY")
-
-#st.title("サンプル")
-#st.write("上から順に表示されます")
-#st.button("ボタン1")
-#st.button("ボタン2")
-
-#st.title("Hello World")
-#st.write("これは Streamlit で表示しているテキストです！")
-
-# データ作成
-#x = np.linspace(0, 10, 100)
-#y = np.sin(x)
-
-# Matplotlib でプロット
-#fig, ax = plt.subplots()
-#ax.plot(x, y)
-#ax.set_title("Sin Wave")
-#ax.set_xlabel("x")
-#ax.set_ylabel("y")
-
-# Streamlit に表示
-#st.pyplot(fig)
