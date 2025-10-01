a9a139b4b3dcc419db440ef85d6b320069a89902
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-10-01 10:54:01 +0900

    Update ode.py

--- odesolver/ode.py
+++ odesolver/ode.py
@@ -6,8 +6,9 @@ def ode_gui():
     line = "例）化学反応の濃度変化(d[A]/dt)は常微分方程式（ODE）で表されます。例えば単純な一次反応なら次のODEに従います。"
     st.write(line)
     st.latex(r"\frac{d [A]}{dt} = -k[A]")
-    st.write("時刻t=0の[A]の値（初期値）を用いてODEを解けば、任意の時刻における[A]を決めることができます。これを初期値問題といいます。")
-    st.write("ODE SolverはODEの式（上式では右辺）と初期値をユーザーが与えることで初期値問題を解くツールです。")
+    line = "時刻t=0の[A]の値（初期値）を用いてODEを解けば、任意の時刻における[A]を決めることができます。これを初期値問題といいます。"
+    line += "ODE SolverはODEの式（上式では右辺）と初期値をユーザーが与えることで初期値問題を解くツールです。"
+    st.write(line)
     st.markdown("---")
     
     text = st.text_area("テキストを入力してください", height=200)
