1ecd9b86017cd643330334f2993c9122046bec6e
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-10-01 10:08:37 +0900

    Update and rename ode_gui.py to ode.py

---
+++ odesolver/ode.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def ode_gui():
+    text = st.text_area("テキストを入力してください", height=200)
+    st.write("入力内容:")
+    st.text(text)
--- odesolver/ode_gui.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-text = st.text_area("テキストを入力してください", height=200)
-st.write("入力内容:")
-st.text(text)
