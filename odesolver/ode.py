2c9197027f02fe4dcc4313ec80f529f10671488f
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-10-01 10:26:57 +0900

    Update ode.py

--- odesolver/ode.py
+++ odesolver/ode.py
@@ -4,7 +4,7 @@ def ode_gui():
     st.markdown("---")
     st.markdown("#### 初期値問題とは？")
     st.write("例）化学反応の濃度変化は常微分方程式（ODE）で表されます。\n 例えば単純な一次反応なら")
-    st.latex(r"\frac{d [A]}{dt} = -k[A}")
+    st.latex(r"\frac{d [A]}{dt} = -k[A]")
     st.markdown("---")
     
     text = st.text_area("テキストを入力してください", height=200)
