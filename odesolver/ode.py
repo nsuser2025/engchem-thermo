6aa12dbf65c97a6ed924836b76640beffc02f0ea
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-10-01 10:14:46 +0900

    Update ode.py

--- odesolver/ode.py
+++ odesolver/ode.py
@@ -1,7 +1,12 @@
 import streamlit as st
 
 def ode_gui():
+    st.markdown("---")
+    st.markdown("#### 常微分方程式ソルバー　カスタムバージョン")
+    #st.latex(r"\eta = \eta_{0}\biggl( 1 - \frac{\phi}{\phi_{\rm max}} \biggr)^{-[\eta]\phi_{\rm max}}")
     st.write("あなたが解きたい初期値問題をコーディングできます。")
+    st.markdown("---")
+    
     text = st.text_area("テキストを入力してください", height=200)
     st.write("入力内容:")
     st.text(text)
