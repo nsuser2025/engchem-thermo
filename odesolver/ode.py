a9605da28038ddce2e663f274522f3d9a3457c6f
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-10-01 10:26:16 +0900

    Update ode.py

--- odesolver/ode.py
+++ odesolver/ode.py
@@ -3,8 +3,8 @@ import streamlit as st
 def ode_gui():
     st.markdown("---")
     st.markdown("#### 初期値問題とは？")
-    #st.latex(r"\eta = \eta_{0}\biggl( 1 - \frac{\phi}{\phi_{\rm max}} \biggr)^{-[\eta]\phi_{\rm max}}")
-    st.write("あなたが解きたい初期値問題を簡単にコーディングできます。")
+    st.write("例）化学反応の濃度変化は常微分方程式（ODE）で表されます。\n 例えば単純な一次反応なら")
+    st.latex(r"\frac{d [A]}{dt} = -k[A}")
     st.markdown("---")
     
     text = st.text_area("テキストを入力してください", height=200)
