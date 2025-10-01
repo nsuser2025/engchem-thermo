b429f95854e23132a23cb2a877c305575f9ee233
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-10-01 13:04:19 +0900

    Update ode_gui2.py

--- odesolver/ode_gui2.py
+++ odesolver/ode_gui2.py
@@ -6,6 +6,15 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 def ode_gui2():
+    
+    st.markdown("---")
+    st.markdown("#### ODE Solver")
+    st.markdown("""例）化学反応の濃度変化(d[A]/dt)は常微分方程式（ODE）で表されます。例えば単純な一次反応なら次のODEに従います。""")
+    st.latex(r"\frac{d [A]}{dt} = -k[A]")
+    st.markdown("""時刻t=0の[A]の値（初期値）を用いてODEを解けば、任意の時刻における[A]を決めることができます。これを初期値問題といいます。
+                 ODE Solverは、ユーザーが入力したODEの式（上式では右辺）、パラメータの値、初期値から初期値問題を解くツールです。""")
+    st.markdown("---")
+    
     # --- ユーザー入力 ---
     st.write("変数を x1, x2, x3 ... のように表し、1行に1つの dXi/dt を入力してください")
     st.write("例: '-k1*x1 + k2*x2'")
