5060c4105c1c2efa4d0d383977dce58daa097e3d
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-10-01 10:43:53 +0900

    Update ode.py

--- odesolver/ode.py
+++ odesolver/ode.py
@@ -3,7 +3,9 @@ import streamlit as st
 def ode_gui():
     st.markdown("---")
     st.markdown("#### 初期値問題とは？")
-    st.write("例）化学反応の濃度変化(d[A]/dt)は常微分方程式（ODE）で表されます。例えば単純な一次反応なら次のODEに従います。")
+    line = "例）化学反応の濃度変化(d[A]/dt)は常微分方程式（ODE）で表されます。例えば単純な一次反応なら次のODEに従います。"
+    #st.write("例）化学反応の濃度変化(d[A]/dt)は常微分方程式（ODE）で表されます。例えば単純な一次反応なら次のODEに従います。")
+    st.write(line)
     st.latex(r"\frac{d [A]}{dt} = -k[A]")
     st.write("時刻t=0の[A]の値（初期値）を用いてODEを解けば、任意の時刻における[A]を決めることができます。これを初期値問題といいます。")
     st.write("ODE SolverはODEの式と初期値をユーザーが与えることで初期値問題を解くツールです。")
