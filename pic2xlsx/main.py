0538c3356e7f810e9663f6a2eb7a91688fa164aa
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-10-02 11:44:15 +0900

    Update main.py

--- pic2xlsx/main.py
+++ pic2xlsx/main.py
@@ -13,10 +13,9 @@ def pic2xlsx_gui():
     if uploaded_file is not None:
        filename = uploaded_file.name
        image = Image.open(uploaded_file)
-       #st.image(image, caption="アップロード画像", width=200)
        col1, col2, col3 = st.columns([1, 2, 1])
        with col2:
-            st.image(image, width=100)
+            st.image(image, caption="アップロード画像", width=100)
     
        # INITIALIZE SESSION_STATE 
        if "df" not in st.session_state:
