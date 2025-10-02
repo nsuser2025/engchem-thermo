12699f425dda83b4cbdf9dbab42d6ddd24858adb
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-10-02 10:15:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,18 +4,18 @@ import numpy as np
 import pandas as pd
 import io
 from visco.kdvisco import kdvisco_gui
-from odesolver.ode import ode_gui
 from odesolver.ode_gui2 import ode_gui2
+from pic2xlsx.main import pic2xlsx
 
 st.image("zkanics_cute_logo.png", caption="Supported by Zkanics F. P. S. since 2024", width=250)
 st.markdown("---")
 
-page = st.selectbox("計算を選択してください", ["SAFT", "KD Viscosity", "ODE Solver", "PNG2XLSX"])
+page = st.selectbox("計算を選択してください", ["SAFT", "KD Viscosity", "ODE Solver", "PIC2XLSX"])
 if page == "SAFT":
      st.header("SAFT Theory")
 elif page == "KD Viscosity":
-     kdvisco_gui()
+     kdvisco_gui ()
 elif page == "ODE Solver":
-     ode_gui2()
-elif page == "PNG2XLSX":
-     st.header("PNG2XLSX")
+     ode_gui2 ()
+elif page == "PIC2XLSX":
+     pic2xlsx ()
