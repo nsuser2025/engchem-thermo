a5ed574764ca67526e73e3fee51c0765a1f20980
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-10-04 16:34:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import io
 from visco.kdvisco import kdvisco_gui
 from odesolver.ode_gui2 import ode_gui2
 from pic2xlsx.main import pic2xlsx_gui
+from lorenz.main import lorenz_gui
 
 st.image("zkanics_cute_logo.png", caption="Supported by Zkanics F. P. S. since 2024", width=250)
 st.markdown("---")
@@ -19,7 +20,7 @@ elif page == "KD Viscosity":
 elif page == "ODE Solver":
      ode_gui2 ()
 elif page == "Lorenz":
-     ode_gui2 ()
+     lorenz_gui ()
 elif page == "PIC2XLSX":
      pic2xlsx_gui ()
 else:
