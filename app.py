c4953deebdbce40de724e8fac9f5b0ccdf5edca7
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-10-04 16:23:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,12 +10,15 @@ from pic2xlsx.main import pic2xlsx_gui
 st.image("zkanics_cute_logo.png", caption="Supported by Zkanics F. P. S. since 2024", width=250)
 st.markdown("---")
 
-page = st.selectbox("計算を選択してください", ["SAFT", "KD Viscosity", "ODE Solver", "PIC2XLSX"])
+select = ["SAFT", "KD Viscosity", "ODE Solver", "Lorenz", "PIC2XLSX"]
+page = st.selectbox("計算を選択してください", select)
 if page == "SAFT":
      st.header("SAFT Theory")
 elif page == "KD Viscosity":
      kdvisco_gui ()
 elif page == "ODE Solver":
+     ode_gui2 ()
+elif page == "Lorenz":
      ode_gui2 ()
 elif page == "PIC2XLSX":
      pic2xlsx_gui ()
