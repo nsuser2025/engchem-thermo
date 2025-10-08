f455300b8addb87a59110f2bc9e3027c42b08f5c
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-10-08 13:22:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import io
+from mkslide.main import mkslide_gui
 from visco.kdvisco import kdvisco_gui
 from odesolver.main import ode_gui
 from pic2xlsx.main import pic2xlsx_gui
@@ -10,15 +11,15 @@ from pic2xlsx.main import pic2xlsx_gui
 st.image("zkanics_cute_logo.png", caption="Supported by Zkanics F. P. S. since 2024", width=250)
 st.markdown("---")
 
-select = ["SAFT", "KD Viscosity", "ODE Solver", "PIC2XLSX"]
+select = ["MKSLIDE", "KD Viscosity", "ODE Solver", "PIC2XLSX"]
 page = st.selectbox("計算を選択してください", select)
-if page == "SAFT":
-     st.header("SAFT Theory")
+if page == "MKSLIDE":
+   mkslide_gui ()
 elif page == "KD Viscosity":
-     kdvisco_gui ()
+   kdvisco_gui ()
 elif page == "ODE Solver":
-     ode_gui ()
+   ode_gui ()
 elif page == "PIC2XLSX":
-     pic2xlsx_gui ()
+   pic2xlsx_gui ()
 else:
      pass
