7fafdcf90736b492f9f6b7d25ac9a0d9136437d3
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-10-08 14:14:18 +0900

    Update main.py

--- mkslide/main.py
+++ mkslide/main.py
@@ -12,6 +12,7 @@ def mkslide_gui():
        df.index = range(1, len(df) + 1)
        st.dataframe(df) 
        exam_input = st.text_input("試験を選んでください（例: A1:A10）", value="A1:A10")
+       face_input = st.text_input("測定面を選んでください（例: A1:A10）", value="A1:A10")
 
        wb = load_workbook(uploaded_file, data_only=True)
        ws = wb.active
@@ -22,13 +23,10 @@ def mkslide_gui():
            data = [[c.value for c in row] for row in cells]
            # DataFrameに変換
            df_range = pd.DataFrame(data)
-           st.success(f"抽出範囲: {exam_input}")
+           st.success(f"試験: {exam_input}")
            st.dataframe(df_range)
        except Exception as e:
            st.error(f"範囲の読み込みに失敗しました: {e}")
-       #test_col = st.selectbox("試験（列）を選択してください", df.columns)
-       #st.write(f"選択された項目: **{test_col}**")
-       #st.dataframe(df[[test_col]])
     else:
        pass
 
