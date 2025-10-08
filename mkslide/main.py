41080106dd0a13b774b7edd9211970dba1729bdc
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-10-08 16:48:56 +0900

    Update main.py

--- mkslide/main.py
+++ mkslide/main.py
@@ -34,6 +34,6 @@ def mkslide_gui():
        selected_elec = st.selectbox("電解液を選んでください", df["電解液"].unique().tolist())
        selected_magn = st.selectbox("倍率を選んでください", df["倍率"].unique().tolist()) 
 
-       result = df.loc[(df["正極"] == selected_cath) & (df["測定面"] == selected_face) & (df["倍率"] == selected_magn), "ファイル名"]
+       result = df.loc[(df["試験"] == selected_exam) & (df["測定面"] == selected_face) & (df["正極"] == selected_cath) & (df["測定"] == selected_mesu) & (df["電解液"] == selected_elec) & (df["倍率"] == selected_magn), "ファイル名"]
        st.write(result)
        
