5669a7253c459b3de1cfff55680bfc7816d406a4
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-10-08 15:13:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import io
-from mkslide.main2 import main_gui2
+from mkslide.main import mkslide_gui
 from visco.kdvisco import kdvisco_gui
 from odesolver.main import ode_gui
 from pic2xlsx.main import pic2xlsx_gui
@@ -14,7 +14,7 @@ st.markdown("---")
 select = ["MKSLIDE", "KD Viscosity", "ODE Solver", "PIC2XLSX"]
 page = st.selectbox("計算を選択してください", select)
 if page == "MKSLIDE":
-   main_gui2 ()
+   mkslide_gui ()
 elif page == "KD Viscosity":
    kdvisco_gui ()
 elif page == "ODE Solver":
