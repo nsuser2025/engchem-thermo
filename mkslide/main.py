55c0cb2a84a6f19a8dd319b1c0fa174a10bb2164
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-10-08 13:26:06 +0900

    Update main.py

--- mkslide/main.py
+++ mkslide/main.py
@@ -3,3 +3,13 @@ import pandas as pd
 
 def mkslide_gui():
     uploaded_file = st.file_uploader("Excelファイルを選択してください", type=["xlsx", "xls"])
+    
+    if uploaded_file is not None:
+       df = pd.read_excel(uploaded_file)
+       st.dataframe(df)
+    else:
+       pass
+
+# MODULE ERROR MESSAGE
+if __name__ == "__main__":
+   raise RuntimeError("Do not run this file directly; use it as a module.")
