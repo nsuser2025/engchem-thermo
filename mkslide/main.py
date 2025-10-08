f1b0e40b8c7fae9101ac3bf0d5d40ed5ac88887a
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-10-08 15:47:57 +0900

    Update main.py

--- mkslide/main.py
+++ mkslide/main.py
@@ -14,10 +14,8 @@ def mkslide_gui():
 　　uploaded_pict = st.file_uploader("画像ファイルを選択してください（複数可）",
                     type=["png", "jpg", "jpeg"], accept_multiple_files=True)
    
-   if uploaded_pict:
-      st.success(f"{len(uploaded_pict)} 件の画像をアップロードしました")
-      for i, uploaded_pict in enumerate(uploaded_pict):
-          image = Image.open(uploaded_file)
-          st.image(image, caption=f"画像 {i+1}: {uploaded_pict.name}", use_column_width=True) 
-    
-
+   #if uploaded_pict:
+   #   st.success(f"{len(uploaded_pict)} 件の画像をアップロードしました")
+   #   for i, uploaded_pict in enumerate(uploaded_pict):
+   #       image = Image.open(uploaded_file)
+   #       st.image(image, caption=f"画像 {i+1}: {uploaded_pict.name}", use_column_width=True) 
