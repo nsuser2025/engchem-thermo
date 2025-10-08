2076bb9f4b6825e3d15e03405a3f192aeec6002a
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-10-08 15:33:32 +0900

    Update main.py

--- mkslide/main.py
+++ mkslide/main.py
@@ -3,10 +3,11 @@ import string
 import pandas as pd
 from openpyxl import load_workbook
 import numpy as np
-#from mkcsv import mkcsv_gui
+from mkcsv import mkcsv_gui
 
 def mkslide_gui():
 
- # ファイルアップロード
- uploaded_file = st.file_uploader("Excel/CSVファイルをアップロード", type=["xlsx", "xls", "csv"])
- option_form = st.radio("MKSLIDEが作成したCSVファイルですか？", ["Yes", "No"], index = 1, horizontal = True)
+    # ファイルアップロード
+    uploaded_file = st.file_uploader("Excel/CSVファイルをアップロード", type=["xlsx", "xls", "csv"])
+    option_form = st.radio("MKSLIDEが作成したCSVファイルですか？", ["Yes", "No"], index = 1, horizontal = True)
+    
