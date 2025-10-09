5835596827abd0fb00b0b9f50e5f1dae20f13abe
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-10-09 11:34:07 +0900

    Update main.py

--- mkslide/main.py
+++ mkslide/main.py
@@ -71,20 +71,21 @@ def mkslide_gui():
                  col_result.image(image, caption=name, use_container_width=True)
 
        if len(result) > 0:
-          prs = Presentation()
-          for name in result:
-              if name in images:
-                 image = images[name]
+          mkpptx_gui(result)
+          #prs = Presentation()
+          #for name in result:
+          #    if name in images:
+          #       image = images[name]
 
-                 tmp_path = f"tmp_{name}"
-                 image.save(tmp_path)
+          #       tmp_path = f"tmp_{name}"
+          #       image.save(tmp_path)
 
-                 slide = prs.slides.add_slide(prs.slide_layouts[5]) 
-                 slide.shapes.add_picture(tmp_path, Inches(1), Inches(1), width=Inches(6))
+          #       slide = prs.slides.add_slide(prs.slide_layouts[5]) 
+          #       slide.shapes.add_picture(tmp_path, Inches(1), Inches(1), width=Inches(6))
 
-          pptx_path = "output.pptx"
-          prs.save(pptx_path)
-          st.success(f"PPTXに画像を追加しました: {pptx_path}")
+          #pptx_path = "output.pptx"
+          #prs.save(pptx_path)
+          #st.success(f"PPTXに画像を追加しました: {pptx_path}")
 
-          with open(pptx_path, "rb") as f:
-               st.download_button("PPTXをダウンロード", f, file_name="output.pptx")
+          #with open(pptx_path, "rb") as f:
+          #     st.download_button("PPTXをダウンロード", f, file_name="output.pptx")
