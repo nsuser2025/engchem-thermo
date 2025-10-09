92db85d1e44980d8bc1b591da79e3de10e8abc95
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-10-09 14:28:42 +0900

    Update main.py

--- mkslide/main.py
+++ mkslide/main.py
@@ -70,20 +70,3 @@ def mkslide_gui():
 
        if len(result) > 0:
           mkpptx_gui(df, images, result)
-          #prs = Presentation()
-          #for name in result:
-          #    if name in images:
-          #       image = images[name]
-
-          #       tmp_path = f"tmp_{name}"
-          #       image.save(tmp_path)
-
-          #       slide = prs.slides.add_slide(prs.slide_layouts[5]) 
-          #       slide.shapes.add_picture(tmp_path, Inches(1), Inches(1), width=Inches(6))
-
-          #pptx_path = "output.pptx"
-          #prs.save(pptx_path)
-          #st.success(f"PPTXに画像を追加しました: {pptx_path}")
-
-          #with open(pptx_path, "rb") as f:
-          #     st.download_button("PPTXをダウンロード", f, file_name="output.pptx")
