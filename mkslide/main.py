3af56087e6123b954ab83cbd3a92490ff5cc1c9b
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-10-10 09:04:57 +0900

    Update main.py

--- mkslide/main.py
+++ mkslide/main.py
@@ -68,7 +68,12 @@ def mkslide_gui():
                  col_result = result_cols[i % 3]
                  col_result.image(image, caption=name, use_container_width=True)
 
-       if len(result) > 0:
+       pptx_choice = ["Yes（ファイル名で配列）",
+                      "Yes（指定条件で配列）",
+                      "No"]
+       pptx_form = st.radio("パワーポイントスライドは作成しますか？", pptx_choice, index = 1, horizontal = True) 
+       #pptx_form = st.radio("パワーポイントスライドは作成しますか？", ["Yes（ファイル名で配列）", "Yes（指定条件で配列）", "No"], index = 1, horizontal = True) 
+       if len(result) > 0 and pptx_form == "Yes（ファイル名で配列）":
           mkpptx_gui(df, images, result)
 
 # MODULE ERROR MESSAGE
