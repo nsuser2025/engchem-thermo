3e4c19f1e17139014d106bf0f57cceea240521c1
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-10-14 08:02:58 +0900

    Update hachida.py

--- mkslide/hachida.py
+++ mkslide/hachida.py
@@ -8,4 +8,16 @@ def condition_selector(df, images, key_prefix=""):
     selected_elec = st.selectbox("電解液を選んでください", ["指定しない"]+df["電解液"].unique().tolist(), key=f"elec_{key_prefix}")
     selected_magn = st.selectbox("倍率を選んでください", ["指定しない"]+df["倍率"].unique().tolist(), key=f"magn_{key_prefix}")
 
-    
+    condition = pd.Series(True, index=df.index)
+    if selected_exam != "指定しない":
+       condition &= (df["試験"] == selected_exam)
+    if selected_face != "指定しない":
+       condition &= (df["測定面"] == selected_face)
+    if selected_cath != "指定しない":
+       condition &= (df["正極"] == selected_cath)
+    if selected_mesu != "指定しない":
+       condition &= (df["測定"] == selected_mesu)
+    if selected_elec != "指定しない":
+       condition &= (df["電解液"] == selected_elec)
+    if selected_magn != "指定しない":
+       condition &= (df["倍率"] == selected_magn)
