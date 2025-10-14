9a34c43f2e656a5206cd56d971740ea816487e6c
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-10-15 08:19:42 +0900

    Update main.py

--- mkslide/main.py
+++ mkslide/main.py
@@ -172,8 +172,15 @@ def mkslide_gui():
     # FILLTERING RESULTS
     final_results = get_filtered_names_by_multiselect_full_order(df, condition_id=condition_id, filter_cols=FILTER_COLS)
 
+    # COUNT HIT IMAGES 
+    if len(final_results) > 0:
+       nhit_image = 0 
+       for j, name in enumerate(final_results):
+           if name in images:
+              nhit_image += 1
+               
     #condition_container.subheader(f"✅ 条件に合致する画像 ({len(final_results)} 件)")
-    condition_container.subheader(f"✅ 条件に合致する画像")
+    condition_container.subheader(f"✅ 条件に合致する画像 ({len(nhit_image)} 件)")
     if len(final_results) == 0:
        condition_container.warning("条件に合致する画像はありません。")
     else:
