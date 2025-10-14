fd07262aa3a4eec06e56ffb3445af42e315db276
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-10-14 12:43:26 +0900

    Update mkcsv.py

--- mkslide/mkcsv.py
+++ mkslide/mkcsv.py
@@ -83,6 +83,7 @@ def mkcsv_gui(uploaded_file):
         df = pd.DataFrame(extracted_data)
         st.success(f"選択範囲を結合したCSVを生成しました。（{list(unique_lengths)[0]}行）")
         st.dataframe(df)
+        retrun df
 
 # MODULE ERROR MESSAGE
 if __name__ == "__main__":
