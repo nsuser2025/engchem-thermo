c79446a4bddfcf5c7c5265c3c7ee07757681f922
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-12-11 13:09:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,18 +4,18 @@ import numpy as np
 import pandas as pd
 import io
 from mkslide.main import mkslide_gui
-from cielab.main_cielab import cielab_gui
+from cielab.cielab_main import cielab_gui
 from odesolver.main import ode_gui
 from pic2xlsx.main import pic2xlsx_gui
 
 st.image("zkanics_cute_logo.png", caption="Supported by Zkanics F. P. S. since 2024", width=250)
 st.markdown("---")
 
-select = ["MKSLIDE", "test", "ODE Solver", "PIC2XLSX"]
+select = ["MKSLIDE", "CIELAB", "ODE Solver", "PIC2XLSX"]
 page = st.selectbox("計算を選択してください", select)
 if page == "MKSLIDE":
    mkslide_gui ()
-elif page == "test":
+elif page == "CIELAB":
    cielab_gui ()
 elif page == "ODE Solver":
    ode_gui ()
