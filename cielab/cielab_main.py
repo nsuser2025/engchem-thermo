f14e625ab1becf91a3f7136f73365581836b98a5
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-12-11 13:14:48 +0900

    Update cielab_main.py

--- cielab/cielab_main.py
+++ cielab/cielab_main.py
@@ -13,7 +13,17 @@ def cielab_gui():
     また、出力されるCSVにはコードインジェクションの無効化処理が施されています。
     安心してダウンロードしてください。""") 
     st.markdown("---")
-    
+
+    # CSV FILE READER
+    uploaded_file = st.file_uploader("Excel/CSVファイルをアップロード", 
+                    type=["xlsx", "xls", "xlsm", "csv"])
+
+    if uploaded_file:
+       if option_form == "No":
+          df = mkcsv_gui(uploaded_file)
+          df = None 
+          st.session_state.data_df = df
+       
 # MODULE ERROR MESSAGE
 if __name__ == "__main__":
    raise RuntimeError("Do not run this file directly; use it as a module.")
