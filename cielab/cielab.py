858c4f4f874b1e7ca7a92fb93f9878e1b5a8a7c0
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-12-12 16:50:17 +0900

    Update cielab.py

--- cielab/cielab.py
+++ cielab/cielab.py
@@ -2,4 +2,11 @@ import streamlit as st
 
 
 def cielab_core (df):
-    st.dataframe(df)
+
+    base_dir = os.path.dirname(__file__)
+    cie_path = os.path.join(base_dir, "CIE_xyz_1931_2deg.csv")
+    ill_path = os.path.join(base_dir, "CIE_std_illum_D65.csv")
+    df_cie = pd.read_csv(cie_path)
+    df_ill = pd.read_csv(ill_path)   
+    
+    st.dataframe(df_cie)
