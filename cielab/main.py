c5473b668f70162a3f12d77aaf3c8e9b8ce2ae2b
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-12-12 10:18:19 +0900

    Update main.py

--- cielab/main.py
+++ cielab/main.py
@@ -37,13 +37,12 @@ def cielab_gui():
     if df is None:
        st.info("データファイル（CSV）をアップロードしてください。")
        return
-    else:
-       base_dir = os.path.dirname(__file__)
-       cie_path = os.path.join(base_dir, "CIE_xyz_1931_2deg.csv")
-       ill_path = os.path.join(base_dir, "CIE_std_illum_D65.csv")
-       df_cie = pd.read_csv(cie_path)
-       df_ill = pd.read_csv(ill_path)
-       st.session_state.data_df = df_cie 
+    
+    base_dir = os.path.dirname(__file__)
+    cie_path = os.path.join(base_dir, "CIE_xyz_1931_2deg.csv")
+    ill_path = os.path.join(base_dir, "CIE_std_illum_D65.csv")
+    df_cie = pd.read_csv(cie_path)
+    df_ill = pd.read_csv(ill_path)
 
     st.markdown("---")
 
