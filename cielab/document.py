19f3b3caa00d5a64d89a56e0b7678c9391fe2336
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-12-15 08:46:29 +0900

    Update document.py

--- cielab/document.py
+++ cielab/document.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def cielab_overview():
     with st.expander("CIE L$^{*}$a$^{*}$b$^{*}$変換の概要"):
-         st.markdown(r"""色の見え方を定量的に扱うために, 色を色空間上の座標として表す方法が用いられます."")
+         st.markdown(r"""色の見え方を定量的に扱うために, 色を色空間上の座標として表す方法が用いられます.""")
          st.markdown(r"""CIE L$^{*}$a$^{*}$b$^{*}$空間では「座標間距離 $\approx$ 人間が感じる色差」が成り立つように設計されています.""")
          #st.markdown()
          #- **L\***：明度（0–100）
