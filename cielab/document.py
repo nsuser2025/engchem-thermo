ff45c88202ee895e6f0785c02bb2aff57c24a1ee
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-12-15 08:39:25 +0900

    Update document.py

--- cielab/document.py
+++ cielab/document.py
@@ -3,7 +3,7 @@ import streamlit as st
 def cielab_overview():
     with st.expander("CIE L$^{*}$a$^{*}$b$^{*}$変換の概要"):
          st.markdown(r"""色の見え方を定量的に扱うために, 色を色空間上の座標として表す方法が用いられます.
-                         L*a*b*座標系は, 座標間距離 $\approx$ 知覚色差""")
+         \[\text{座標間距離} \approx \text{人間が感じる色差}\]""")
          #- **L\***：明度（0–100）
          #- **a\***：赤（＋）↔ 緑（−）
          #- **b\***：黄（＋）↔ 青（−）
