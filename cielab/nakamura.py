12c015f693964f9fd58c2c99bc1184f112a1c5e6
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-12-16 07:54:15 +0900

    Create nakamura.py

---
+++ cielab/nakamura.py
@@ -0,0 +1,14 @@
+
+### MAXIMUM AND MINIMUM FINDER ###
+def max_min_finder (wl, vals):
+    peaks_pos, _ = find_peaks(vals)
+    peaks_neg, _ = find_peaks(-vals)
+    return peaks_pos, peaks_neg
+
+### POINTS OF MAX SLOPE ###
+def remove_background (wl, vals):
+    peaks_pos, peaks_neg = max_min_finder (wl, vals)
+    peaks_all = np.sort(np.concatenate([peaks_pos, peaks_neg]))
+    for i in peaks_pos:
+    return peaks_all
+
