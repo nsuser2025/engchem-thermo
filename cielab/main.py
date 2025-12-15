32050e6f89e0f6f35f3ac4cdfa7effed00315dd5
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-12-15 10:20:47 +0900

    Update main.py

--- cielab/main.py
+++ cielab/main.py
@@ -68,7 +68,7 @@ def cielab_gui():
            a_vals += [ai]
            b_vals += [bi]
        dist_w = np.sqrt((L_vals - Lw)**2 + (a_vals - aw)**2 + (b_vals - bw)**2)
-       df_lab = pd.DataFrame({"L*": L_vals, "a*": a_vals, "b*": b_vals}, "Distance_to_ideal_white": dist_w)
+       df_lab = pd.DataFrame({"L*": L_vals, "a*": a_vals, "b*": b_vals, "Distance_to_ideal_white": dist_w})
        df_lab_safe = sanitize_for_csv_injection(df_lab) 
        st.dataframe(df_lab_safe)   
        st.markdown("---") 
