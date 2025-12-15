beb8a709e9536baccadcd18c44a13ed285142e0f
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-12-15 10:13:32 +0900

    Update cielab.py

--- cielab/cielab.py
+++ cielab/cielab.py
@@ -4,7 +4,6 @@ import pandas as pd
 import numpy as np
 from scipy.interpolate import interp1d, CubicSpline
 import matplotlib.pyplot as plt
-from .mkcsv import  
 
 def load_measurements (df):
     wl = df.iloc[:,0].to_numpy(dtype=float)
