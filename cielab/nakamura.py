676caea82ea1c0d2dd312be2f44983e99a3602f8
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-12-16 11:46:52 +0900

    Update nakamura.py

--- cielab/nakamura.py
+++ cielab/nakamura.py
@@ -8,69 +8,17 @@ def max_min_finder (wl, vals):
     peaks_neg, _ = find_peaks(-vals)
 
     wl_pos = wl[peaks_pos]
-    vals_pos = vals[peaks_pos]
-    mask = wl_pos < 380
-    if np.any(mask):
-       min_pos_pre = peaks_pos[mask][np.argmax(wl_pos[mask])]
-    else:
-       min_pos_pre = None
-    mask = wl_pos >= 380
-    if np.any(mask):
-       min_pos = peaks_pos[mask][np.argmin(wl_pos[mask] - 380)]
-    else:
-       min_pos = None 
-
     wl_neg = wl[peaks_neg]
+
+    vals_pos = vals[peaks_pos]
     vals_neg = vals[peaks_neg]
-    mask = wl_neg < 380
-    if np.any(mask):
-       min_neg_pre = peaks_neg[mask][np.argmax(wl_neg[mask])]
-    else:
-       min_neg_pre = None
-    mask = wl_neg >= 380
-    if np.any(mask):
-       min_neg = peaks_neg[mask][np.argmin(wl_neg[mask] - 380)]
-    else:
-       min_neg = None 
     
-    if wl[min_pos] > wl[min_neg] and min_pos_pre != None:
-       idx_ini = min_pos_pre 
-       wl_ini = wl[min_pos_pre]
-       vals_ini = vals[min_pos_pre]
-    elif wl[min_neg] > wl[min_pos] and min_neg_pre != None:
-       idx_ini = min_neg_pre
-       wl_ini = wl[min_neg_pre]
-       vals_ini = vals[min_neg_pre] 
-
     mask = (wl_pos >= 380) & (wl_pos <= 780)
-    wl_pos_range = wl_pos[mask]
-    vals_pos_range = vals_pos[mask]
-    peaks_pos_range = peaks_pos[mask]
+    wl_cast = wl_pos[mask]
+    vals_cast = vals_pos[mask]
+    peaks_cast = peaks_pos[mask]
 
-    mask = (wl_neg >= 380) & (wl_neg <= 780)
-    wl_neg_range = wl_neg[mask]
-    vals_neg_range = vals_neg[mask]
-    peaks_neg_range = peaks_neg[mask]
-    
-    wl_sum = np.sort(np.concatenate([[wl_ini], wl_pos_range, wl_neg_range]))
-    vals_sum = np.sort(np.concatenate([[vals_ini], vals_pos_range, vals_neg_range])) 
-    peaks_sum = np.concatenate([[idx_ini], peaks_pos_range, peaks_neg_range])
-
-    #st.write('sarusaru')
-    #st.write(wl_pos_range)
-    #st.write(wl_neg_range)
-    #st.write(wl_sum)
-    #st.write(vals_sum)
-    
-    order = np.argsort(wl_sum)
-    wl_nakamura = wl_sum[order]
-    vals_nakamura = vals_sum[order]
-    peaks_nakamura = peaks_sum[order]
-    st.write('zkanics')
-    #st.write(wl_nakamura)
-    #st.write(vals_nakamura)
-    
-    return wl_nakamura, vals_nakamura, peaks_nakamura
+    return wl_cast, vals_cast, peaks_cast
 
 ### POINTS OF MAX SLOPE ###
 def remove_background (wl, vals):
