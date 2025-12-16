99a63ec223b5708a41ec1ebf9a7c6d7f4c792088
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-12-16 13:45:31 +0900

    Update nakamura.py

--- cielab/nakamura.py
+++ cielab/nakamura.py
@@ -61,6 +61,8 @@ def max_min_finder (wl, vals):
 
 ### LINEAR SPECTRUM GENERATOR ###
 def linear_spectrum (wl_grid, vals_i, wl_maxmin, vals_maxmin):
+    for i in range(len(wl_maxmin)-1):
+        st.write(wl_maxmin(i))
     vals_cast = vals_maxmin
     return vals_cast
     
