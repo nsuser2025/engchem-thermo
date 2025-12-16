7fdcf636c82d397b194102f7995c8adfc3e182b9
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-12-16 13:53:35 +0900

    Update nakamura.py

--- cielab/nakamura.py
+++ cielab/nakamura.py
@@ -61,14 +61,18 @@ def max_min_finder (wl, vals):
 
 ### LINEAR SPECTRUM GENERATOR ###
 def linear_spectrum (wl_grid, vals_i, wl_maxmin, vals_maxmin):
-    x_mid = []
+    wl_mid = []
+    vals_mid = []
     for i in range(len(wl_maxmin)-1):
-        x_mid_value = 0.50 * (wl_maxmin[i] + wl_maxmin[i+1])
-        mask = wl_grid <= x_mid_value
+        wl_mid_value = 0.50 * (wl_maxmin[i] + wl_maxmin[i+1])
+        mask = wl_grid <= wl_mid_value
         idx_in_mask = np.argmax(wl_grid[mask])
-        x_mid += [wl_grid[idx_in_mask]]
-    x_mid = np.array(x_mid)    
-    st.write(x_mid)
+        wl_mid += [wl_grid[idx_in_mask]]
+        vals_mid += [vals_i[idx_in_mask]]
+    wl_mid = np.array(wl_mid)
+    vals_mid = np.array(vals_mid)
+    st.write(wl_mid)
+    st.write(vals_mid)
     vals_cast = vals_maxmin
     return vals_cast
     
