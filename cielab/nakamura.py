6b6dffa1bff5ee0a03e0596bcf1c8f90c35257fa
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-12-16 09:53:39 +0900

    Update nakamura.py

--- cielab/nakamura.py
+++ cielab/nakamura.py
@@ -12,12 +12,12 @@ def max_min_finder (wl, vals):
     min_pos_pre = peaks_pos[mask][np.argmax(wl_pos[mask])]
     mask = wl_pos >= 380
     min_pos = peaks_pos[mask][np.argmin(wl_pos[mask] - 380)]
-    mask = wl_pos <= 780
-    max_pos = peaks_pos[mask][np.argmax(wl_pos[mask])]
+    #mask = wl_pos <= 780
+    #max_pos = peaks_pos[mask][np.argmax(wl_pos[mask])]
     st.write(wl_pos)
     st.write(wl[min_pos_pre])
     st.write(wl[min_pos])
-    st.write(wl[max_pos])
+    #st.write(wl[max_pos])
 
     #wl_neg = wl[peaks_neg]
     #mask = wl_neg >= 380
