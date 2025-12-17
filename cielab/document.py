6ed5f1fccde979c54b635c90d245ed5db1025016
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-12-17 09:35:04 +0900

    Update document.py

--- cielab/document.py
+++ cielab/document.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 def cielab_overview():
+    
     with st.expander("CIE L$^{*}$a$^{*}$b$^{*}$変換の概要"):
          st.markdown(r"""色の見え方を定量的に扱うために, 色を色空間上の座標として表す方法が用いられます.  
                          CIE L$^{*}$a$^{*}$b$^{*}$空間では「座標間距離 $\approx$ 人間が感じる色差」
@@ -15,10 +16,18 @@ def cielab_overview():
                          これは, 380 nm未満, 780 nmを超える波長は人間が色として知覚できないためです.""")
          st.markdown(r"""参考として刺激値色空間の座標X, Y, Zも出力しますが, CIE L$^{*}$a$^{*}$b$^{*}$空間とは対照的に  
                         「座標間距離 $\approx$ 人間が感じる色差」は成り立ちません.""")
+    
     with st.expander("CIE L$^{*}$a$^{*}$b$^{*}$変換の計算式"):
          st.latex(r"L^{\ast} = 116 F(Y/Y_{n}) - 16")
          st.latex(r"a^{\ast} = 500 ( F(X/X_{n}) - F(Y/Y_{n}))")
          st.latex(r"b^{\ast} = 200 ( F(Y/Y_{n}) - F(Z/Z_{n}))")
+    
+    with st.expander("黄色度（Yellow Index, YI）の計算式"):
+         st.markdown(r"""D65光源""")
+         st.latex(r"YI = 100 ()/")
+         st.markdown(r"""C光源""") 
+         st.latex(r"YI = 100 (1.2985X-1.1335Z) / Y")
+              
         
 # MODULE ERROR MESSAGE
 if __name__ == "__main__":
